da4467e655f3f5d295e92bcb89179da1acc926cf
kisslas2020 <pinewood82@gmail.com>
2021-03-24 15:45:24 +0100

    finish

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        Integer res = 0;
+
+        for(Integer num : numbers) {
+            if(num % 2 != 0) {
+                res += num;
+            }
+        }
+        return res;
     }
 }
